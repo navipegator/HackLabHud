c57a761102bb6b45038de64097949843b6f937eb
navipegator <petri@kisut.com>
2021-06-07 18:39:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express')
  var bodyParser = require('body-parser')
 const app = express();
+/*app.use(bodyParser.raw({
+  type: 'image/png',
+  limit: '10mb'
+}));*/
+/*
+app.use(bodyParser.raw({ inflate: true, limit: '1000kb', type: '' }));
+*/
 const fs = require('fs');
+var image;
 app.get('/', (req, res) => {
 	console.log("get");
-  return res.send('Received a GET HTTP method');
+  return res.send('<img src="download">');
 });
  
-app.post('/', (req, res) => {
-console.log("post");
-console.log(req);
+app.post('/upload', (req, res) => {
+image = req.body;
+console.log(image);
 return res.send('Received a POST HTTP method');
 });
  
@@ -36,26 +44,26 @@ const handleError = (err, res) => {
     .contentType("text/plain")
     .end("Oops! Something went wrong!");
 };
-
+app.get('/download', (req, res) => {
+res.set('Content-Type', 'image/png')
+  return res.send(image);
+});
+ /*
 const upload = multer({
   dest: "./"
   // you might also want to set some limits: https://github.com/expressjs/multer#limits
 });
-
+*/
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
 
 app.post(
-  "/upload",
+  "/upload1",
   upload.single("file" /* name attribute of <file> element in your form */),
   (req, res) => {
-	  console.log("saving:" + req.file.originalname);
-	        fs.rename(req.file.path, req.file.originalname, err => {
-        if (err) return handleError(err, res);
+	  console.log("saving:" + Object.keys(req.file));
+	  image = req.file.buffer;
 
-        res
-          .status(200)
-          .contentType("text/plain")
-          .end("File uploaded!");
-      });
 
   }
 );
