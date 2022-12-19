493c0faeeb14ef94a7a3a86e07c10ffda578f672
Petri Kultanen <petri.kultanen@mediconsult.fi>
2022-12-19 12:48:00 +0200

    cleanup camera

--- app/src/main/java/com/smartsalo/hacklab1/CameraPreviewActivity.java
+++ app/src/main/java/com/smartsalo/hacklab1/CameraPreviewActivity.java
@@ -60,44 +60,59 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
+ * Draws camera preview on the screen, and with buttons, sends the preview or imagecapture to server, or saved to sdcard.
+ * Author: Petri Kultanen
  */
 public class CameraPreviewActivity extends AppCompatActivity {
-    private int REQUEST_CODE_PERMISSIONS = 1001;
+    // Permissions to use camera and save pictures.
+    private final int REQUEST_CODE_PERMISSIONS = 1001;
     private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+    // Taking picture requires executor to do it.
     private Executor executor = Executors.newSingleThreadExecutor();
+    // ImageCapture needed by Camera
     private ImageCapture imageCapture;
+    // Preview View
     PreviewView mPreviewView;
-    ImageView captureImage;
+    // Preview size
+    private final int PREVIEWHEIGHT = 480;
+    private final int PREVIEWWIDTH = 800;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Hide action bar
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
+        // Initialize preview and camera
         setContentView(R.layout.activity_camera_preview);
         mPreviewView = findViewById(R.id.previewView);
-
         if(allPermissionsGranted()){
             startCamera(); //start camera if permission has been granted by user
         } else{
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
-            Log.i("p1","Permissions not granted");
+            Log.i("HackHud","Permissions not granted");
         }
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
+        if(requestCode == REQUEST_CODE_PERMISSIONS){
+            if(allPermissionsGranted()){
+                startCamera();
+            } else{
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                this.finish();
+            }
+        }
+    }
     private void startCamera() {
-
         final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-
         cameraProviderFuture.addListener(new Runnable() {
             @Override
             public void run() {
                 try {
-
                     ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
                     bindPreview(cameraProvider);
 
@@ -111,19 +126,13 @@ public class CameraPreviewActivity extends AppCompatActivity {
 
     void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
 
-        Preview preview = new Preview.Builder().setTargetResolution(new Size(800,480))
+        Preview preview = new Preview.Builder().setTargetResolution(new Size(PREVIEWWIDTH,PREVIEWHEIGHT))
                .build();
-
-
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
-
         ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
                 .build();
-
-
-
 
         preview.setSurfaceProvider(mPreviewView.createSurfaceProvider());
         ImageCapture.Builder builder = new ImageCapture.Builder();//.setTargetRotation(Surface.ROTATION_180);
@@ -131,13 +140,11 @@ public class CameraPreviewActivity extends AppCompatActivity {
                 .setTargetRotation(/*this.getWindowManager().getDefaultDisplay().getRotation()*/Surface.ROTATION_0)
                 .build();
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview, imageAnalysis, imageCapture);
-       // Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview, imageAnalysis, imageCapture);
-        // Create output options object which contains file + metadata
+        cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview, imageAnalysis, imageCapture);
 
     }
-    public void sendFullPicture(){
-        Log.i("Hacklab","SendFullPicture");
+    private void sendFullPicture(){
+        Log.i("HackHud","SendFullPicture");
         ImageCapture.Builder builder = new ImageCapture.Builder();//.setTargetRotation(Surface.ROTATION_180);
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
@@ -145,13 +152,13 @@ public class CameraPreviewActivity extends AppCompatActivity {
         imageCapture.takePicture (outputOptions,executor,new ImageCapture.OnImageSavedCallback() {
                     @Override
                     public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                        Log.i("Hacklab","Uploadinf");
+                        Log.i("HackHud","Uploadinf");
                         uploadImage(stream.toByteArray());
                     }
 
                     @Override
                     public void onError(ImageCaptureException error) {
-                        Log.e("Hacklab",error.getMessage());
+                        Log.e("HackHud",error.getMessage());
                         // insert your code here.
                     }
                 }
@@ -159,7 +166,7 @@ public class CameraPreviewActivity extends AppCompatActivity {
 
     }
     public void sendPreviewPicture(){
-        uploadBitmap(mPreviewView.getBitmap());
+        uploadImage(getFileDataFromDrawable(mPreviewView.getBitmap()));
     }
 
     public String getBatchDirectoryName() {
@@ -183,35 +190,18 @@ public class CameraPreviewActivity extends AppCompatActivity {
         }
         return true;
     }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
-        if(requestCode == REQUEST_CODE_PERMISSIONS){
-            if(allPermissionsGranted()){
-                startCamera();
-            } else{
-                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
-                this.finish();
-            }
-        }
-    }
     private void uploadImage(final byte[] image) {
 
         //getting the tag from the edittext
         final String tags = "tageja";
 
         //our custom volley request
-        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, "http://65.109.137.148/upload1",
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, "http://65.109.137.146/upload1",
                 new Response.Listener<NetworkResponse>() {
                     @Override
                     public void onResponse(NetworkResponse response) {
-                      /*  try {
-                            JSONObject obj = new JSONObject(new String(response.data));
-                            Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }*/
-                        Log.i("p1",response.data.toString());
+                        Log.i("HackHud",response.data.toString());
                     }
                 },
                 new Response.ErrorListener() {
@@ -243,60 +233,6 @@ public class CameraPreviewActivity extends AppCompatActivity {
                 long imagename = System.currentTimeMillis();
 
                 params.put("file", new DataPart(imagename + ".png",image));
-                return params;
-            }
-        };
-
-        //adding the request to volley
-        Volley.newRequestQueue(this).add(volleyMultipartRequest);
-    }
-    private void uploadBitmap(final Bitmap bitmap) {
-
-        //getting the tag from the edittext
-        final String tags = "tageja";
-
-        //our custom volley request
-        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, "http://65.109.137.147:81/upload1",//http://65.21.104.214:81/upload
-                new Response.Listener<NetworkResponse>() {
-                    @Override
-                    public void onResponse(NetworkResponse response) {
-                      /*  try {
-                            JSONObject obj = new JSONObject(new String(response.data));
-                            Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }*/
-                        Log.i("p1",response.data.toString());
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }) {
-
-            /*
-             * If you want to add more parameters with the image
-             * you can do it here
-             * here we have only one parameter with the image
-             * which is tags
-             * */
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                //params.put("file", tags);
-                return params;
-            }
-
-            /*
-             * Here we are passing image by renaming it with a unique name
-             * */
-            @Override
-            protected Map<String, DataPart> getByteData() {
-                Map<String, DataPart> params = new HashMap<>();
-                long imagename = System.currentTimeMillis();
-                params.put("file", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
                 return params;
             }
         };
